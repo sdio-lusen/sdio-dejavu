cdf0b9e8349acefc3162047bc9784c784a6e8a78
sdio-lusen <lusen@sdio.co.jp>
2025-10-09 11:11:30 +0000

    build: update setup.py for sdio_dejavu package rename and metadata

--- setup.py
+++ setup.py
@@ -14,22 +14,14 @@ def parse_requirements(requirements):
         return reqs
 
 
-PACKAGE_NAME = "PyDejavu"
-PACKAGE_VERSION = "0.1.3"
-SUMMARY = 'Dejavu: Audio Fingerprinting in Python'
+PACKAGE_NAME = "sdio-dejavu"
+PACKAGE_VERSION = "0.1.3-sdio"
+SUMMARY = "SDIO-Dejavu: Customized audio fingerprinting for CM detection"
 DESCRIPTION = """
-Audio fingerprinting and recognition algorithm implemented in Python
+Customized version of Dejavu for TV Commercial (CM) detection.
 
-See the explanation here:
-
-`http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/`__
-
-Dejavu can memorize recorded audio by listening to it once and fingerprinting
-it. Then by playing a song and recording microphone input or on disk file,
-Dejavu attempts to match the audio against the fingerprints held in the
-database, returning the song or recording being played.
-
-__ http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/
+Based on the original project by Will Drevo:
+http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/
 """
 REQUIREMENTS = parse_requirements("requirements.txt")
 
@@ -38,15 +30,15 @@ setup(
     version=PACKAGE_VERSION,
     description=SUMMARY,
     long_description=DESCRIPTION,
-    author='Will Drevo',
-    author_email='will.drevo@gmail.com',
-    maintainer="Will Drevo",
-    maintainer_email="will.drevo@gmail.com",
-    url='http://github.com/worldveil/dejavu',
+    author='Lusen Kong',
+    author_email='lusen@sdio.co.jp',
+    maintainer="Lusen Kong",
+    maintainer_email="lusen@sdio.co.jp",
+    url='https://github.com/sdio-lusen/sdio-dejavu',
     license='MIT License',
     include_package_data=True,
-    packages=find_packages(),
-    platforms=['Unix'],
+    packages=find_packages(include=["sdio_dejavu", "sdio_dejavu.*"]),
+    platforms=["Unix", "MacOS", "Windows"],
     install_requires=REQUIREMENTS,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -56,5 +48,5 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords="python, audio, fingerprinting, music, numpy, landmark",
+    keywords="python, audio, fingerprinting, music, dejavu, sdio, cm detection",
 )
